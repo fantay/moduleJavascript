bddecbfc78b3839af1d379c8bdd721c6198e7f68
fantay <laurentcoll@hotmail.com>
2016-10-12 16:33:24 +0200

    exercice jeu de recherche d'un nombre aleatoire

--- public_html/JS/app.js
+++ public_html/JS/app.js
@@ -4,14 +4,31 @@
  * and open the template in the editor.
  */
 
-var obj = {
-    titre : "Kung fu panda",
-    anneeProd : 2000,
-    nbLiens : 20,
-    realisateurs : [{nom:'disney'},{nom:'pixar'}],
-    affiche : function(){
-        return alert(this.titre + " " + this.anneeProd + " " + this.nbLiens + " " + this.realisateurs[0].nom + " " + this.realisateurs[1].nom);
-    }
-};
+//var obj = {
+//    titre : "Kung fu panda",
+//    anneeProd : 2000,
+//    nbLiens : 20,
+//    realisateurs : [{nom:'disney'},{nom:'pixar'}],
+//    affiche : function(){
+//        return this.titre + " " + this.anneeProd + " " + this.nbLiens + " " + this.realisateurs[0].nom + " " + this.realisateurs[1].nom;
+//    }
+//};
+//alert(obj.affiche());
 
-obj.affiche();
+var calnbr = Math.random() * 100 + 1;
+var nbrchercher = Math.floor(calnbr);
+var nbrtour = 0;
+var nbrrentre;
+
+do {
+    nbrtour++;
+    nbrrentre = prompt("Tours " + nbrtour + "\nSaississez votre nombre :");
+    if (nbrrentre > nbrchercher) {
+        alert("c'est plus petit");
+    } else if (nbrrentre < nbrchercher) {
+        alert("c'est plus grand");
+    }
+} while (nbrrentre != nbrchercher)
+
+alert("c'est gagné en " + nbrtour + " tour(s)");
+
