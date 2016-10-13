d1b2402b9c870cd76fa69c23e15c09f63599a5f6
fantay <laurentcoll@hotmail.com>
2016-10-13 11:00:19 +0200

    deuxieme boutton avec saisie de texte

--- public_html/JS/evenements.js
+++ public_html/JS/evenements.js
@@ -5,3 +5,20 @@
  */
 
 
+function clicBoutton(){
+    var titre = document.getElementById("titre");
+    var menu = document.getElementsByClassName("menu");
+    
+    
+    titre.innerHTML = "yo Man!!!!";
+    
+    for ( i=0; i<menu.length; i++){
+        var tag = menu[i];
+        tag.innerHTML = "Peace";
+    }
+}
+
+function clicBoutton2(){
+    var saisie = document.getElementById("texte").value;
+    document.getElementById("titre").innerHTML = saisie.toUpperCase();
+}
